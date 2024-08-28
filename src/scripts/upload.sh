1336d8ee64b64f64912935fd6cd851752602405a
Léo Ecrepont <57994579+lecrepont01@users.noreply.github.com>
2024-08-28 13:14:34 +0200

    chore(ci): test upload script (#9)
    Finish testing script in CI and validate orb.
    
    Fixes MRGFY-3970
    
    Depends-On: #6

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# This example uses envsubst to support variable substitution in the string parameter type.
-# https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
-
-#TO=$(circleci env subst "${PARAM_TO}")
-# If for any reason the TO variable is not set, default to "World"
-#echo "Hello ${TO:-World}!"
-
 set -x
+
 if [[ $REPO_URL =~ ^https:\/\/github\.com\/([a-zA-Z0-9._-]+)\/([a-zA-Z0-9._-]+)$ ]]; then
   REPO_FULL_NAME=${BASH_REMATCH[1]}/${BASH_REMATCH[2]}
 else
@@ -25,10 +19,6 @@ curl -X POST \
   -o result.json \
   ${MERGIFY_API_SERVER}/repos/${REPO_FULL_NAME}/ci_issues_upload \
 
-echo "Display result"
-cat result.json
-
 GIGID=$(cat result.json | jq -r .gigid)
 echo "::notice title=CI Issues report::CI_ISSUE_GIGID=$GIGID"
-
-#            echo "CI_ISSUE_GIGID=$GIGID" >> "$GITHUB_OUTPUT"
+echo "CI_ISSUE_GIGID=$GIGID" >> "$GITHUB_OUTPUT"
